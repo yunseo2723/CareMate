a01030914114fe16c3fba2b2d226e9c23d2e30c9
하 윤서 <yunseo2723@naver.com>
2025-10-10 01:28:38 +0900

    Postgres에서 적용되지 않는 테이블명 변경

--- backend/src/main/java/me/hys/carematebackend/model/User.java
+++ backend/src/main/java/me/hys/carematebackend/model/User.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
