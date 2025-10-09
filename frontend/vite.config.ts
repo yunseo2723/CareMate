58298eabeb03ba9d87fdcb79de3765a8768d47f2
하 윤서 <yunseo2723@naver.com>
2025-10-10 03:01:39 +0900

    netlify 배포 문제 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   server: { proxy: { '/api': 'http://localhost:8080' } }
 
