79ed66a5dd32fc6769b191a86dc3b8f54d2e2d92
하 윤서 <yunseo2723@naver.com>
2025-12-17 22:01:12 +0900

    테스트 환경설정 변경

--- backend/src/test/java/me/hys/carematebackend/CareMateBackendApplicationTests.java
+++ backend/src/test/java/me/hys/carematebackend/CareMateBackendApplicationTests.java
@@ -1,21 +1,11 @@
 package me.hys.carematebackend;
 
-import me.hys.carematebackend.service.ai.AiRecommendService;
-import me.hys.carematebackend.service.ai.OpenAiClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class CareMateBackendApplicationTests {
     @Test
     void contextLoads() {
     }
-    @MockitoBean
-    AiRecommendService aiRecommendService;
-
-    @MockitoBean
-    OpenAiClient openAiClient;
 }
