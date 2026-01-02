eefb0b52f7ceefb39469ab09e48d96493c2f580d
하 윤서 <yunseo2723@naver.com>
2026-01-03 01:03:57 +0900

    CORS 에러

--- ml-serving/app/main.py
+++ ml-serving/app/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .schemas import RecommendResponse
 from .recommend.service import recommend_top_facilities
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://caremate.kro.kr",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/recommend/top", response_model=RecommendResponse)
